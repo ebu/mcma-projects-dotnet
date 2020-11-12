101bfa67ec0d5925c09aaff3e5528b99255342cc
Evan Verney-Fink <evan@triskel-inc.com>
2020-11-12 08:29:55 -0500

    Initial work on .NET Google Cloud sample

---
+++ simple-google-cloud/services/JobProcessor/Mcma.GoogleCloud.JobProcessor.Common/CloudSchedulerJobCheckerTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Google.Cloud.Scheduler.V1;
+using Mcma.Utility;
+
+namespace Mcma.GoogleCloud.JobProcessor.Common
+{
+    public class CloudSchedulerJobCheckerTrigger : IJobCheckerTrigger
+    {
+        private CloudSchedulerClient CloudSchedulerClient { get; } = CloudSchedulerClient.Create();
+
+        public Task EnableAsync() => CloudSchedulerClient.ResumeJobAsync(McmaEnvironmentVariables.Get("CLOUD_SCHEDULER_JOB_NAME"));
+
+        public Task DisableAsync() => CloudSchedulerClient.PauseJobAsync(McmaEnvironmentVariables.Get("CLOUD_SCHEDULER_JOB_NAME"));
+    }
+}
